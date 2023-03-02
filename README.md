3bc0c973ab40390ee1f9af9c0f3fda601b6e9384
benson <rhualikescode@gmail.com>
2023-03-02 13:27:09 +0800

    Init

--- README.md
+++ README.md
@@ -1,11 +1,9 @@
-## Template for node.js express + mongoose db
+## Template for Express + TypeScript + nodemon
 
 #### How to use it
 
-1. git clone.
-2. Create .env file.
-3. Copy the connect config from `cloud.mongodb.com`(Atlas Mongo DB). (ex: `mongodb+srv://username:<password>@project-name.ejb9xzv.mongodb.net/?retryWrites=true&w=majority`)
-4. Change the password that you copied from.
-5. start:nodemon to start the server.
+1. `git clone.`
+2. `yarn install`
+3. `yarn start:nodemon` to start the server.
 
 **Note:** ofc you can change the DB as you want, basically it's express + ts + nodemon for the server.
--- server.ts
+++ server.ts
@@ -1,7 +1,6 @@
 import express from "express";
 import { Request, Response } from "express";
 import dotenv from "dotenv";
-import mongoose from "mongoose";
 
 const app = express();
 
@@ -11,19 +10,6 @@ app.get("/", (req: Request, res: Response) => {
 
 dotenv.config();
 
-const connect = () => {
-  mongoose.set("strictQuery", false);
-  mongoose
-    .connect(`${process.env.MONGO_DB}`)
-    .then(() => {
-      console.log("connect to mongodb database");
-    })
-    .catch((err) => {
-      throw err;
-    });
-};
-
 app.listen(8000, () => {
-  connect();
   console.log("Application started on port 8000!");
 });
